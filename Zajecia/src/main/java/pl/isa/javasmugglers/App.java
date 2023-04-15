7b26be68f0051f0df86fd6ed3800eec099068546
Blazej-Jendrzejewski <blazej.jendrzejewski@gmail.com>
2023-04-15 07:51:09 +0200

    Dodano testowy kod wyświetlający użytkowników w bazie

--- Zajecia/src/main/java/pl/isa/javasmugglers/App.java
+++ Zajecia/src/main/java/pl/isa/javasmugglers/App.java
@@ -3,6 +3,7 @@ package pl.isa.javasmugglers;
 
 public class App {
     public static void main(String[] args) {
+        //testowe odpalenie aplikacji
         System.out.println("WITAMY W MENAGERZE ZAJĘĆ");
         MainMenu mainMenu = new MainMenu();
         while (true) {
