24d2b97c6ad4be981cc66c1689df19fe3bbdc838
Blazej-Jendrzejewski <blazej.jendrzejewski@gmail.com>
2023-04-16 09:18:17 +0200

    male fixy

--- Zajecia/src/main/java/pl/isa/javasmugglers/App.java
+++ Zajecia/src/main/java/pl/isa/javasmugglers/App.java
@@ -5,9 +5,11 @@ public class App {
     public static void main(String[] args) {
         System.out.println("WITAMY W MENAGERZE ZAJĘĆ");
         MainMenu mainMenu = new MainMenu();
-        while (true) {
+        boolean isOn = true;
+        while (isOn) {
             System.out.println(mainMenu.getMenu());
             mainMenu.userSelection(mainMenu.getValidInput());
         }
     }
 }
+
