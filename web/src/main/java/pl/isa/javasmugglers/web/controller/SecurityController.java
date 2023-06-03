ab236e8bc5b62643a891c4e00064c67ef457bb5e
MikSob4523 <mikolaisobolewski@gmail.com>
2023-06-03 12:22:14 +0200

    Logowanie - zwraca id użytkownika

--- web/src/main/java/pl/isa/javasmugglers/web/controller/SecurityController.java
+++ web/src/main/java/pl/isa/javasmugglers/web/controller/SecurityController.java
@@ -1,20 +1,26 @@
 package pl.isa.javasmugglers.web.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import pl.isa.javasmugglers.web.model.user.User;
+import pl.isa.javasmugglers.web.repository.UserRepository;
 
 import java.security.Principal;
 
 @Controller
+@AllArgsConstructor
 public class SecurityController {
-
+    private final UserRepository userRepository;
     @RequestMapping(value = "/succeslogin.html", method = RequestMethod.GET)
     @ResponseBody
-    public String currentUserNameSimple(HttpServletRequest request) {
+    public Long currentUserNameSimple(HttpServletRequest request) {
         Principal principal = request.getUserPrincipal();
-        return principal.getName();
+
+        return userRepository.findByEmail(principal.getName()).get().getId();
     }
 }
