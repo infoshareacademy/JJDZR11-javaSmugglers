eccce1763e25a8dc9084de702866fffd36f8981c
MikSob4523 <mikolaisobolewski@gmail.com>
2023-06-17 15:16:16 +0200

    usunięto nieużywaną metodę z UserService

--- web/src/main/java/pl/isa/javasmugglers/web/service/UserService.java
+++ web/src/main/java/pl/isa/javasmugglers/web/service/UserService.java
@@ -1,7 +1,6 @@
 package pl.isa.javasmugglers.web.service;
 
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -17,22 +16,7 @@ public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    public String signUpUser(User user) {
-        boolean userExists = userRepository
-                .findByEmail(user.getUsername())
-                .isPresent();
-        if (userExists) {
-            throw new IllegalStateException("email already taken");
-        }
-        String encodedPassword = passwordEncoder
-                .encode(user.getPassword());
-        user.setPassword(encodedPassword);
 
-        userRepository.save(user);
-
-        // TODO:SEND CONFIRMATION And tokken
-        return "Użytkownik  " + user.getFirstName() + " " + user.getLastName() + " został zarejestrowany. Proszę czekać na akceptacje użytkownika.";
-    }
 
     private static final String USER_NOT_FOUND = "USER WITH EMAIL %S NOT FOUND.";
 
