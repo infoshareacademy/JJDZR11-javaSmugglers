07af87758dff301db300aabc762448f73f96cee4
Blazej-Jendrzejewski <blazej.jendrzejewski@gmail.com>
2023-06-17 19:44:17 +0200

    fixed security issue with logo.gif

--- web/src/main/java/pl/isa/javasmugglers/web/security/config/SecurityConfiguration.java
+++ web/src/main/java/pl/isa/javasmugglers/web/security/config/SecurityConfiguration.java
@@ -37,7 +37,7 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests()
                                 .requestMatchers("registration/**", "registration/professor/**")
                                 .permitAll()
-                                .requestMatchers("/rf/**","/login/**","register/**", "/save/**","/registrationsuccesfull/**", "/", "/addnew/**")
+                                .requestMatchers("/rf/**","/login/**","register/**", "/save/**","/registrationsuccesfull/**", "/", "/addnew/**","/logo.gif")
                                 .permitAll()
                 .anyRequest()
                 .authenticated().and()
